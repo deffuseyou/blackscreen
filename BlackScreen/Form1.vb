86f4748984a7a0220cf7913800691c77834fff98
Kriulin Dmitry <kriulindmitry@mail.ru>
2023-12-19 04:01:50 +0300

    Добавьте файлы проекта.

---
+++ BlackScreen/Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Windows.Forms
+Public Class BS
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+        Me.BackColor = Color.Black
+        System.Windows.Forms.Cursor.Hide()
+    End Sub
+End Class
